f7c4879fcbc4c4952ffac8646c06fdd212dbd22f
saurabh11y <saurabhji399@gmail.com>
2025-10-14 13:05:35 +0530

    negative slicing

--- chapter _03/01_intro_to_string.py
+++ chapter _03/01_intro_to_string.py
@@ -2,6 +2,6 @@ a = "harry"  # ya name = "harry"
 
 name = a[0:4] # start from index 0 all the way till 3 (excluding 3)
 print(name)
-
+# second method
 character1 = name[1]
 print(character1) 
---
+++ chapter _03/02_nagative_slicing.py
@@ -0,0 +1,10 @@
+name = "harry"
+
+print(name [0:3])
+
+print(name[-4:-1])  # negative hai to isko positive main karo
+print(name[1:4])
+
+print(name[:3])    # is same as print(name[0:3])
+print(name[1:])    # is same as print(name[1:4])
+print(name[1:5])
