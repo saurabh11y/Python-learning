6020d3b5ce398f3505f1313a18feaa1321cbefa6
saurabh11y <saurabhji399@gmail.com>
2025-10-14 13:38:10 +0530

    string function

---
+++ chapter _03/03_str_function.py
@@ -0,0 +1,11 @@
+name = "harry"
+
+print(len(name))
+
+print(name.endswith("rry"))
+print(name.endswith("rrya"))
+
+print(name.startswith("h"))
+print(name.capitalize())
+
+print(name.replace("r","y"))
