c4fd4cb1c22c2a740adae81640312d39bdb83b20
saurabh11y <saurabhji399@gmail.com>
2025-10-15 13:28:51 +0530

    list

--- chapter_04/01_lists.py
+++ chapter_04/01_lists.py
@@ -1,6 +1,11 @@
 friends=["harry","shani","varun",11,17,True,False]
 
 print(friends[3]) 
-friends[2] = "thakur"
 
+friends[2] = "thakur"  # lists are mutable
 print(friends[2])
+
+
+# same as string slice, in there we call indexing
+print(friends[1:7])
+print(friends[1:6:2])
---
+++ chapter_04/02_list_methods.py
@@ -0,0 +1,14 @@
+friends=["harry","shani","varun",11,17,True,False]
+print(friends) 
+
+friends.append("yadavji")
+print(friends)
+
+l1 =[1,6,8,9,7,15,11,3]
+# sort()
+# pop(5)
+# reverse()
+# append(8)
+# insert(3,70)
+l1.remove(15) 
+print(l1)
