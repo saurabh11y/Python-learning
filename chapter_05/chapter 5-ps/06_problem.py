8d2350257b3b7a1db2305b69249d210f1a999ef8
saurabh11y <saurabhji399@gmail.com>
2025-10-24 20:31:37 +0530

    ps 6

---
+++ chapter_05/chapter 5-ps/06_problem.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter Friends name:")
+lang = input("Enter language name:")
+d.update({name:lang})
+
+name = input("Enter Friends name:")
+lang = input("Enter language name:")
+d.update({name:lang})
+
+name = input("Enter Friends name:")
+lang = input("Enter language name:")
+d.update({name:lang})
+
+name = input("Enter Friends name:")
+lang = input("Enter language name:")
+d.update({name:lang})
+
+print(d)
