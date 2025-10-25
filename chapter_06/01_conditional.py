91796e417d8efac1609646577fa99087f7f7592e
saurabh11y <saurabhji399@gmail.com>
2025-10-25 13:48:13 +0530

    if else statement

---
+++ chapter_06/01_conditional.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age:"))
+
+# If else statement
+if(a>=18):
+    print("You are eligble for vote")
+    print("and deiving licence to ")
+
+else:
+    print("You are not eligble for vote")
+
+print("end of peogram")
---
+++ chapter_06/02_if_elif_else_ladder.py
@@ -0,0 +1,28 @@
+a = int(input("Enter your age:"))
+
+# If elif else statement
+if(a>=18):
+    print("You are eligble for vote")
+    print("and driving licence to ")
+
+elif(a<0):
+    print("You entering an invalid negative age ")
+
+elif(a==0):
+    print("You entering an invalid age ")
+
+else:
+    print("You are not eligble for vote")
+
+print("end of peogram")
+
+
+
+
+# d = int(input("Enter a number:"))
+# if(d>=9):
+#     print("Greater")
+# else:
+#     print("smallest")
+
+# print(d)      
