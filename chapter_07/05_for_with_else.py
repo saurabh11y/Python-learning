8adf0ee4cf0b5bd8e90d072b28924431051d4806
saurabh11y <saurabhji399@gmail.com>
2025-10-29 15:18:41 +0530

    for with else

---
+++ chapter_07/05_for_with_else.py
@@ -0,0 +1,6 @@
+l = [1,4,5,6,9,]
+
+for item in l:
+    print(item)
+else:
+    print("done")    # this is print when loops exits
---
+++ chapter_07/06_break_&_continue.py
@@ -0,0 +1,13 @@
+# break statement
+for i in range(100):
+    if(i == 50):
+        break     # exit the loop
+    print(i)
+
+
+
+# continue statement
+for i in range(100):
+    if(i == 50):
+        continue  # skip the iteration
+    print(i)
