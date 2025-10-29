bbd33e696b12176d7f35c5623d14978bd3cbeed0
saurabh11y <saurabhji399@gmail.com>
2025-10-29 16:33:53 +0530

    chapter 7 done

--- chapter_07/chapter_4-ps/09_problem.py
+++ chapter_07/chapter_4-ps/09_problem.py
@@ -2,8 +2,8 @@ n = int(input("Enter the number: "))
 for i in range(1, n+1):
     if(i==1 or i==n):
        print("*"* n, end="")
-  else:
-     print("*", end="")
-     print(" "* (n-2), end="")
-    print("*", end="")
-print("")
+    else:
+          print("*", end="")
+          print(" "* (n-2), end="")
+          print("*", end="")
+    print("")
