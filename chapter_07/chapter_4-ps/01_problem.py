4698756e2577bdafc2973c35f92d1ad1019c4fc2
Saurabh Yadav <saurabhji399@gmail.com>
2025-10-29 16:24:46 +0530

    Update 01_problem.py

--- chapter_07/chapter_4-ps/01_problem.py
+++ chapter_07/chapter_4-ps/01_problem.py
@@ -2,3 +2,23 @@ n = int(input("Enter a number:"))
 
 for i in range(1,11):
     print(f"{n} X {i} = {n*i}")
+
+
+n = int(input("Enter the number: "))
+
+for i in range(1, n+1):
+
+if(i==1 or i==n):
+
+print("*"* n, end="")
+
+else:
+
+print("*", end="")
+
+print(" "* (n-2), end="")
+
+print("*", end="")
+
+
+print("")
