76eb318f4bce3854d81441d629a140552aef3659
saurabh11y <saurabhji399@gmail.com>
2025-11-01 15:18:30 +0530

    function

---
+++ chapter_08/01_function.py
@@ -0,0 +1,20 @@
+# function defination
+def func1():
+    print("Hello")
+   
+  # function call 
+func1()    
+func1()
+
+
+def avg():
+    a =int(input("Enter Your number:"))
+    b =int(input("Enter Your number:"))
+    c =int(input("Enter Your number:"))
+
+    average =(a+b+c)/3
+    print(average)
+
+avg()
+print("thank you")
+avg()
---
+++ chapter_08/02_function_argument.py
@@ -0,0 +1,10 @@
+def goodDay(name,ending):
+    print("Good Day," + name)
+    print(ending)
+    return "done"
+
+
+goodDay("shauraya","bye")    
+    
+a = goodDay("varun","bye")    
+print(a)
