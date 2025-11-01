353602014a8d43441463f2392070e62b3f6d8f20
saurabh11y <saurabhji399@gmail.com>
2025-11-01 15:44:30 +0530

    recursion

---
+++ chapter_08/03_deafult_argument.py
@@ -0,0 +1,6 @@
+def goodDay(name,ending = "Thank you"):     # niche defini nikiya to thank you by deaful lelega
+    print("Good Day," + name)
+    print(ending)
+
+goodDay("shani","thanks")
+goodDay("varun",)
