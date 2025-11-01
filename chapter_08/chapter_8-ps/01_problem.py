53be666e26006d77209123ab042deb9992b7f8e6
saurabh11y <saurabhji399@gmail.com>
2025-11-01 18:39:26 +0530

    ps

---
+++ chapter_08/chapter_8-ps/01_problem.py
@@ -0,0 +1,11 @@
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>a and c>b):
+        return c
+a = 32
+b = 211
+c = 9
+print(greatest(a,b,c))
