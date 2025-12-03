f91f62aa4a44aa47bc5dddb1d2b2a202e2ddc53a
saurabh11y <saurabhji399@gmail.com>
2025-12-03 20:25:17 +0530

    college home work

---
+++ college practise/1.py
@@ -0,0 +1,13 @@
+a = 78
+b = 87
+c = 45
+
+print("Marks in maths:",a)
+print("Marks in science:",b)
+print("Marks in english:",c)
+
+total = (a+b+c)
+print(total)
+
+percentage = (total/300)*100
+print(percentage)
